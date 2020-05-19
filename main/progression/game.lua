76d7218a33ac161bcf2db250f6bdeb76ea550b41
Marius Petcu <dapetcu21@gmail.com>
2020-05-19 23:58:59 +0300

    Add progressions

---
+++ main/progression/game.lua
@@ -0,0 +1,5 @@
+local function game()
+  print("The game's main progression starts here")
+end
+
+return game
---
+++ main/progression/main.go
@@ -0,0 +1,15 @@
+components {
+  id: "main"
+  component: "/main/progression/main.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
